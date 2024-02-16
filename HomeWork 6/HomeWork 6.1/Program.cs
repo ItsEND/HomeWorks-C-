0e5ac8bcca99155d33edcf15f27f20f28f8906c3
Rezvich <waweda299@gmail.com>
2024-02-16 16:16:50 +0300

    Домашка 6

---
+++ HomeWork 6/HomeWork 6.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6._1
+{
+	internal class Program
+	{
+		/*
+		Два треугольника заданы длинами своих сторон. 
+		Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+		Для решения задачи можно использовать формулу Герона 
+		 */
+		static double GetS(double x, double y, double z)
+		{
+			double p = (x + y + z) / 2;
+			return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите стороны первого триугольника");
+			double a = Convert.ToDouble(Console.ReadLine());
+			double b = Convert.ToDouble(Console.ReadLine());
+			double c = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите стороны второго триугольника");
+			double a2 = Convert.ToDouble(Console.ReadLine());
+			double b2 = Convert.ToDouble(Console.ReadLine());
+			double c2 = Convert.ToDouble(Console.ReadLine());
+			double firstS = GetS(a, b, c);
+			double secondS = GetS(a2, b2, c2);
+			if (firstS > secondS)
+			{
+				Console.WriteLine("Первый и он равен {0:.00}", firstS);
+			}
+			else if (firstS < secondS) { Console.WriteLine("Второй и он равен {0:.00}", secondS); }
+			else { Console.WriteLine("Ранвны {0:.00}", secondS); }
+			Console.ReadKey();
+		}
+	}
+}
---
+++ HomeWork 6/HomeWork 6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6._2
+{
+	internal class Program
+	{
+		/*
+		Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+		*/
+		static void GetParamSphere(double a, out double S, out double V)
+		{
+			S = 6 * a * a;
+			V = Math.Pow(a, 3);
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите длину ребра куба");
+			double a = Convert.ToDouble(Console.ReadLine());
+			double S;
+			double V;
+			GetParamSphere(a, out S, out V);
+			Console.WriteLine("Площадь полной поверхности куба равна {0:.00}, а объем равен {1:.00}",S,V);
+			Console.ReadLine();
+		}
+	}
+}
