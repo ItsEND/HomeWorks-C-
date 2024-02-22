ee65219a5b2a334fb6f1f7c97a55bb0e97ed7723
Rezvich <waweda299@gmail.com>
2024-02-22 12:15:17 +0300

    HomeWork 7

---
+++ HomeWork 7/HomeWork 7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Выберите любую папку на своем компьютере, имеющую вложенные директории. 
+			Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+			*/
+			string path = "D:/NewFile";
+			string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+			foreach (string file in files)
+			{
+				Console.WriteLine(file);
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ HomeWork 7/HomeWork 7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Программно создайте текстовый файл и запишите в него 10 случайных чисел.
+			//Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.
+			string path = "D:/numbers.txt";
+			Random rand = new Random();
+			int sum = 0;
+			using (StreamWriter sw = new StreamWriter(path, false))
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					sw.WriteLine(rand.Next(-10, 10));
+				}
+			}
+			using (StreamReader sr = new StreamReader(path))
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					int a = Convert.ToInt32(sr.ReadLine());
+					sum += a;
+				}
+			}
+			Console.WriteLine(sum);
+			Console.ReadLine();
+		}
+	}
+}
