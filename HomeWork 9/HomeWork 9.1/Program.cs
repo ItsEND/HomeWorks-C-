9213a0a60949e26e2754585526350964ccc7c336
Rezvich <waweda299@gmail.com>
2024-02-25 15:26:34 +0300

    HomeWork 9

---
+++ HomeWork 9/HomeWork 9.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_9._1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Transform transform;
+
+
+			//Угол задан с помощью целочисленных значений
+			//gradus - градусов, min - угловых минут, sec - угловых секунд.
+			//Реализовать класс, в котором указанные значения представлены в виде свойств.
+			//Для свойств предусмотреть проверку корректности данных.
+			//Класс должен содержать конструктор для установки начальных значений,
+			//а также метод ToRadians для перевода угла в радианы.
+			//Создать объект на основе разработанного класса.
+			//Осуществить использование объекта в программе.
+			try
+			{
+				
+					Console.WriteLine("Введите градусы");
+					int gradus = Convert.ToInt32(Console.ReadLine());
+
+					Console.WriteLine("Введите минуты");
+					int min = Convert.ToInt32(Console.ReadLine());
+
+					Console.WriteLine("Введите секунды");
+					int sec = Convert.ToInt32(Console.ReadLine());
+
+					transform = new Transform(gradus, min, sec);
+			
+				
+
+				double ToRadians = transform.ToRadians();
+
+				Console.WriteLine("Радианы равны {0:.00000}", ToRadians);
+			}
+			catch (Exception ex) { Console.WriteLine(ex.Message); }
+            Console.ReadLine();
+
+
+		}
+	}
+}
---
+++ HomeWork 9/HomeWork 9.1/Transform.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_9._1
+{
+	internal class Transform
+	{
+		int gradus;
+		int min;
+		int sec;
+		public int Gradus
+		{
+			get
+			{
+				return gradus;
+			}
+			set
+			{
+				gradus = value % 24;
+			}
+		}
+		public int Min
+		{
+			get
+			{
+				return min;
+			}
+			set
+			{
+				Gradus += value / 60;
+				min = value % 60;
+			}
+		}
+		public int Sec
+		{
+			get
+			{
+				return sec;
+			}
+			set
+			{
+				Min += value / 60;
+				sec = value % 60;
+			}
+		}
+		public Transform(int gradus,int min,int sec)
+		{
+			this.gradus = gradus;
+			this.min = min;
+			this.sec = sec;
+		}
+
+		public double ToRadians()
+		{
+			return (sec / 3600 + min / 60 + gradus) * (Math.PI / 180);
+		}
+	}
+}
