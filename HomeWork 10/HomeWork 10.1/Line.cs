72ff42ead5b72d6ceb758ffe072f4f0518de65e3
Rezvich <waweda299@gmail.com>
2024-02-26 14:05:49 +0300

    HomeWork 10

---
+++ HomeWork 10/HomeWork 10.1/Line.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_10._1
+{
+	struct Line
+	{
+		double k;
+		double b;
+
+		public Line(double k, double b)
+		{
+			this.k = k;
+			this.b = b;
+		}
+
+		public string Root()
+		{
+			if (k== 0 && b!=0)
+			{
+				return "Нет корней";
+			}
+
+			double x = -b/k;
+			return $"Отевет - {x}";
+			
+		}
+
+
+	}
+}
---
+++ HomeWork 10/HomeWork 10.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_10._1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Разработать структуру для решения линейного уравнения 0=kx+b.
+			//Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+			//Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры.
+			//Осуществить использование экземпляра в программе.
+			Console.WriteLine("Введите k");
+			double k = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите b");
+			double b = Convert.ToDouble(Console.ReadLine());
+
+			Line line = new Line(k, b);
+
+			Console.WriteLine(line.Root());
+			Console.ReadKey();
+
+
+		}
+	}
+}
