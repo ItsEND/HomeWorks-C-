d9bcf25d2f87913139b0edc832a5395ad60a8c85
Rezvich <waweda299@gmail.com>
2024-02-29 18:36:57 +0300

    HomeWork 13

---
+++ HomeWork 13/HomeWork 13/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_13
+{
+	abstract class Animal
+	{
+		public abstract string Name { get; set; }
+		public Animal(string name)
+		{
+			Name = name;
+		}
+		public abstract void Say();
+		public void ShowInfo()
+		{
+			Console.Write("Меня зовут {0} и я очень люблю говорить ",Name);
+			Say();
+        }
+	}
+}
